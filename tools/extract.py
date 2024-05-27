620ffe28689f41d7830dbb5883f5106513b8d6dd
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2024-05-26 23:23:49 -0700

    Transform data working

--- tools/extract.py
+++ tools/extract.py
@@ -69,6 +69,79 @@ def jsonObjFromUnityFile(unityPath):
         byFileId[fileID] = data;
     return byFileId;
 
+def sceneThreeFromJsonScene(component_by_file_id, object_by_guid):
+    by_types = {};
+    root_transform = None;
+    by_file_id = component_by_file_id;
+
+    def typeNameAndObject(typedObj):
+        first_key = list(typedObj.keys())[0];
+        first_type = first_key;
+        first_val = typedObj[first_key];
+        return (first_type,first_val);
+
+    for (index,file_id) in enumerate(component_by_file_id):
+        file_obj = component_by_file_id[file_id];
+        first_key = list(file_obj.keys())[0];
+        first_type = first_key;
+        first_val = file_obj[first_key];
+        if (not(first_type in by_types)):
+            by_types[first_type] = [];
+        by_types[first_type].append(first_val);
+    
+    def getFileId(file_id):
+        if (isinstance(file_id,dict) and 'fileID' in file_id):
+            file_id = file_id['fileID'];
+        id = str( file_id );
+        if (id == "0"):
+            return None;
+        with_type = by_file_id[id];
+        (type_name,obj) = typeNameAndObject(with_type);
+        return obj;
+    def getPtr(obj,prop):
+        return getFileId(obj[prop]);
+    def listFromDictVector(vecDict):
+        ans = [];
+        for k in vecDict:
+            v = vecDict[k];
+            ans.append(v);
+        return ans;
+    
+    transTypeName = "Transform";
+    assert(transTypeName in by_types);
+    all_transforms = by_types[transTypeName];
+    result_scenes = [];
+    for transform in all_transforms:
+        transform['out_index'] = len(result_scenes);
+        gameObj = getPtr(transform,'m_GameObject');
+        scene = { 
+            'name':gameObj['m_Name'],
+            'children':[],
+            "position":listFromDictVector(transform['m_LocalPosition']),
+            "quaternion":listFromDictVector(transform['m_LocalRotation']),
+            "scale":listFromDictVector(transform['m_LocalScale']),
+        };
+        result_scenes.append(scene);
+    
+
+    def resultSceneByFileId(file_id):
+        src = getFileId(file_id);
+        if (src is None): return None;
+        dst = result_scenes[src['out_index']]
+        return dst;
+    
+    # link across scenes now:
+    for (index,transform) in enumerate( all_transforms ):
+        toScene = result_scenes[index];
+        for child in transform['m_Children']:
+            childResult = resultSceneByFileId(child['fileID'])
+            toScene['children'].append(childResult);
+        fatherScene = resultSceneByFileId(transform['m_Father']['fileID']);
+        if (fatherScene is None):
+            root_obj = toScene;
+
+    return root_obj;
+
 #print(jsonObjFromUnityFile("UnityProject/VRBiofield/Assets/ChiVR_MainApp_Chakras.unity"));
 
 
@@ -83,7 +156,7 @@ def filesInFolder(path):
         for file in files:
             lastDot = file.rfind(".");
             if (lastDot < 0):
-                assert(False);
+                #assert(False);
                 continue;
             ext = file[lastDot:];
             isMeta = (ext in knownYamlExtensions);
@@ -127,12 +200,14 @@ if (not fileExists(finalAllMetaFile)):
 
 def ensureJsonFromUnity(unityPath):
     jsonPath = unityPath + ".json";
-    if (fileExists(jsonPath)):
-        return jsonPath;
+    #if (fileExists(jsonPath)): return jsonPath;
     obj = jsonObjFromUnityFile(unityPath);
+    obj = sceneThreeFromJsonScene(obj, None);
     writeJsonToFile(obj, jsonPath);
     return jsonPath;
-ensureJsonFromUnity("UnityProject/VRBiofield/Assets/ChiVR_MainApp_Chakras.unity");
+
+fileToExport = "UnityProject/VRBiofield/Assets/BiofieldCore/ModelPerson/Yogi/Yoga Pose.prefab"
+ensureJsonFromUnity(fileToExport);
 
 print("Done.");
 
